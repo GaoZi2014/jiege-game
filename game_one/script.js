560d37f26a1ef1b768714c90249b366e7ecbd161
Jiege <122613317+GaoZi2014@users.noreply.github.com>
2024-03-05 08:59:53 +0800

    Add files via upload

---
+++ game_one/script.js
@@ -0,0 +1,32 @@
+function fail() {
+    const text = document.getElementById("failed")
+    const textd = document.getElementById("failed-d")
+    const container = document.getElementById
+    ("container")
+    const it = document.getElementById("it")
+    const title = document.getElementById("title")
+
+    text.style.display = "block";
+    textd.style.display = "block";
+    container.style.display = "none";
+    it.style.backgroundColor = "white";
+    it.style.color = "black";
+    title.style.display = "none";
+}
+
+function won() {
+    const text = document.getElementById("won")
+    const textd = document.getElementById("won-d")
+    const container = document.getElementById
+    ("container")
+    const it = document.getElementById("it")
+    const title = document.getElementById("title")
+
+    text.style.display = "block";
+    textd.style.display = "block";
+    container.style.display = "none";
+    it.style.backgroundColor = "white";
+    it.style.color = "black";
+    title.style.display = "none";
+}
+
